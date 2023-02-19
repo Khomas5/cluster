387e4afabdc00f4ae9e5f329dbe377220ef09ba6
unknown <ana.khomasuridze.2@iliauni.edu.ge>
2023-02-19 21:09:24 +0400

    in progress of course page

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,56 +1,20 @@
-function createCalendar(year, month) {
-    // Create a new Date object for the given year and month
-    const date = new Date(year, month - 1);
-  
-    // Get the number of days in the given month
-    const numDays = new Date(year, month, 0).getDate();
-  
-    // Get the day of the week of the first day in the given month
-    const firstDay = date.getDay();
-  
-    // Create an array to hold the calendar days
-    const days = [];
-  
-    // Add empty cells to the beginning of the array to align the first day
-    for (let i = 0; i < firstDay; i++) {
-      days.push("");
+var calendar = new Calendar(calendarEl, {
+  initialDate: '2014-11-10',
+  initialView: 'timeGridWeek',
+  events: [
+    {
+      start: '2014-11-10T10:00:00',
+      end: '2014-11-10T16:00:00',
+      display: 'background'
     }
-  
-    // Add the days of the month to the array
-    for (let i = 1; i <= numDays; i++) {
-      days.push(i);
-    }
-  
-    // Add empty cells to the end of the array to complete the calendar grid
-    while (days.length % 7 !== 0) {
-      days.push("");
-    }
-  
-    // Create a table element to hold the calendar grid
-    const table = document.createElement("table");
-  
-    // Create a header row for the calendar
-    const headerRow = table.insertRow();
-    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    for (let i = 0; i < 7; i++) {
-      const cell = headerRow.insertCell();
-      cell.textContent = daysOfWeek[i];
-    }
-  
-    // Create a row for each week in the calendar
-    let row;
-    for (let i = 0; i < days.length; i++) {
-      if (i % 7 === 0) {
-        row = table.insertRow();
-      }
-      const cell = row.insertCell();
-      cell.textContent = days[i];
-    }
-  
-    // Add the table to the calendar container element
-    const calendarContainer = document.getElementById("calendar");
-    calendarContainer.appendChild(table);
-  }
-  
-  // Call the createCalendar function to generate the calendar for February 2023
-  createCalendar(2023, 2);
+  ]
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+  });
+  calendar.render();
+});
+
